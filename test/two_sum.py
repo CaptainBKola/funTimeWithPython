948000c39f8171c6bc4f258d438808204cad8948
Badmus Kola <31166533+CaptainBKola@users.noreply.github.com>
2021-10-21 01:29:02 +0100

    Myersbriggs in python

---
+++ test/two_sum.py
@@ -0,0 +1,22 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)  # add assertion here
+
+    def test_sum(self):
+        pass
+        #given
+
+
+        #when
+
+        #test
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
